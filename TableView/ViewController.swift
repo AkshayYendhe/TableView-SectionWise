5eb41c30b03af6d1af5c5aa01c41c5369b4b2014
forloop <yendheakshayd@gmail.com>
2023-02-03 11:21:32 +0530

    TableView SectionWise

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let names = [["John Smith", "1111111"],
+                 ["Nik Fury","2222222"],
+                 ["Paul Walkaer","3333333"],
+                 ["James Bond","44444444"]]
+    
+    let names2 = ["a","b","c","d", "e"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You Tapped me!")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return names.count
+        }
+        return names2.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var text = ""
+        if indexPath.section == 0 {
+        let name = names[indexPath.row]
+            text = "\(name[0]) \(name[1])"
+        } else {
+            text = names2[indexPath.row]
+        }
+        cell.textLabel?.text = text
+        return cell
+    }
+    
 }
+
 
